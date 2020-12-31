8bbd1c48a2840b7c0dfac9c5e7843e350ccf6fc5
UsamaKashif <49620321+UsamaKashif@users.noreply.github.com>
2021-01-01 01:14:00 +0500

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,40 +1,41 @@
 #include<iostream>
 #include<stdlib.h>
-#include<windows.h>
 #include<string>
+#include <windows.h>>
 using namespace std;
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 
 char wonBy_ticTacToe;
 
-unsigned int microsecond = 1000000;
 
 
 void reset_sqaures () {
-    square[0] = '0';
-    square[1] = '1';
-    square[2] = '2';
-    square[3] = '3';
-    square[4] = '4';
-    square[5] = '5';
-    square[6] = '6';
-    square[7] = '7';
-    square[8] = '8';
-    square[9] = '9';
+    square[0] = '1';
+    square[1] = '2';
+    square[2] = '3';
+    square[3] = '4';
+    square[4] = '5';
+    square[5] = '6';
+    square[6] = '7';
+    square[7] = '8';
+    square[8] = '9';
 }
 
 int checkwin()
 {
 
-    if (square[1] == square[2] && square[2] == square[3])
+    if (square[0] == square[1] && square[1] == square[2])
         return 1;
 
-    else if (square[4] == square[5] && square[5] == square[6])
+    else if (square[3] == square[4] && square[4] == square[5])
         return 1;
 
-    else if (square[7] == square[8] && square[8] == square[9])
+    else if (square[6] == square[7] && square[7] == square[8])
+        return 1;
+
+    else if (square[0] == square[3] && square[3] == square[6])
         return 1;
 
     else if (square[1] == square[4] && square[4] == square[7])
@@ -43,18 +44,15 @@ int checkwin()
     else if (square[2] == square[5] && square[5] == square[8])
         return 1;
 
-    else if (square[3] == square[6] && square[6] == square[9])
+    else if (square[0] == square[4] && square[4] == square[8])
         return 1;
 
-    else if (square[1] == square[5] && square[5] == square[9])
+    else if (square[2] == square[4] && square[4] == square[6])
         return 1;
 
-    else if (square[3] == square[5] && square[5] == square[7])
-        return 1;
-
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
-             && square[4] != '4' && square[5] != '5' && square[6] != '6'
-             && square[7] != '7' && square[8] != '8' && square[9] != '9')
+    else if (square[0] != '1' && square[1] != '2' && square[2] != '3'
+             && square[3] != '4' && square[4] != '5' && square[5] != '6'
+             && square[6] != '7' && square[7] != '8' && square[8] != '9')
         return 0;
 
     else
@@ -113,40 +111,38 @@ bool tictactoe ()
 
             mark = (player == 1) ? 'X' : 'O';
 
-            if (choice == 1 && square[1] == '1')
+            if (choice == 1 && square[0] == '1')
+
+                square[0] = mark;
+            else if (choice == 2 && square[1] == '2')
 
                 square[1] = mark;
-            else if (choice == 2 && square[2] == '2')
+            else if (choice == 3 && square[2] == '3')
 
                 square[2] = mark;
-            else if (choice == 3 && square[3] == '3')
+            else if (choice == 4 && square[3] == '4')
 
                 square[3] = mark;
-            else if (choice == 4 && square[4] == '4')
+            else if (choice == 5 && square[4] == '5')
 
                 square[4] = mark;
-            else if (choice == 5 && square[5] == '5')
+            else if (choice == 6 && square[5] == '6')
 
                 square[5] = mark;
-            else if (choice == 6 && square[6] == '6')
+            else if (choice == 7 && square[6] == '7')
 
                 square[6] = mark;
-            else if (choice == 7 && square[7] == '7')
+            else if (choice == 8 && square[7] == '8')
 
                 square[7] = mark;
-            else if (choice == 8 && square[8] == '8')
+            else if (choice == 9 && square[8] == '9')
 
                 square[8] = mark;
-            else if (choice == 9 && square[9] == '9')
-
-                square[9] = mark;
             else
             {
                 cout << "Invalid move ";
 
                 player--;
-                cin.ignore();
-                cin.get();
             }
             i = checkwin();
 
